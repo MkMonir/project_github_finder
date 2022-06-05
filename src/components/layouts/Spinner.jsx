3cb49e466ec4b9099be6a7d0f974340df2e2ac08
MkMonir <khanmonir1335@gmail.com>
2022-06-06 00:47:58 +0600

    create project layouts

---
+++ src/components/layouts/Spinner.jsx
@@ -0,0 +1,10 @@
+import spinner from './../layouts/assets/spinner.gif';
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img width={180} src={spinner} alt="Loading..." className="text-center mx-auto" />
+    </div>
+  );
+}
+export default Spinner;
